493de6c37e93cee043b1354d9f30aae335e0e1bc
Dhruv Mehta <flashxx001@gmail.com>
2024-07-07 21:54:08 +0530

    Changed width.

--- src/App.js
+++ src/App.js
@@ -6,28 +6,36 @@ const messages = [
 
 export default function App() {
   const step = 3;
+
+  function handlePrevious() {
+    alert("previous");
+  }
+  function handleNext() {
+    alert("next");
+  }
+
   return (
     <div className="h-screen text-white bg-light-gray flex items-center justify-center ">
-      <div className="p-12 w-[600px] h-fit flex flex-col gap-8 items-center justify-between bg-zinc-200">
+      <div className="p-12 w-[600px] max-w-[90%] h-fit flex flex-col gap-8 items-center justify-between bg-zinc-200">
         <div className="flex items-center justify-between w-full text-black ">
           <div
             className={`${
-              step >= 1 ? "bg-custom-purple" : "bg-zinc-300"
-            } rounded-full  w-12 font-semibold text-lg h-12 flex items-center justify-center`}
+              step === 1 ? "bg-custom-purple" : "bg-zinc-300"
+            } rounded-full  w-12 font-semibold text-lg h-12 flex items-center justify-center cursor-pointer`}
           >
             1
           </div>
           <div
             className={`${
-              step >= 1 ? "bg-custom-purple" : "bg-zinc-300"
-            } rounded-full  w-12 font-semibold text-lg h-12 flex items-center justify-center`}
+              step === 2 ? "bg-custom-purple" : "bg-zinc-300"
+            } rounded-full  w-12 font-semibold text-lg h-12 flex items-center justify-center `}
           >
             2
           </div>
           <div
             className={`${
-              step >= 1 ? "bg-custom-purple" : "bg-zinc-300"
-            } rounded-full  w-12 font-semibold text-lg h-12 flex items-center justify-center`}
+              step === 3 ? "bg-custom-purple" : "bg-zinc-300"
+            } rounded-full  w-12 font-semibold text-lg h-12 flex items-center justify-center cursor-pointer`}
           >
             3
           </div>
@@ -39,12 +47,17 @@ export default function App() {
           <button
             style={{ backgroundColor: "#7950f2" }}
             className="font-semibold text-sm px-3 py-2 rounded-md"
+            onClick={handlePrevious}
+            // onMouseEnter={() => {
+            //   alert("Mouse has Entered.");
+            // }}
           >
             Previous
           </button>
           <button
             style={{ backgroundColor: "#7950f2" }}
             className="font-semibold text-sm px-3 py-2 rounded-md"
+            onClick={handleNext}
           >
             Next
           </button>
