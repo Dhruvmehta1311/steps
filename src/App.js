03f8e7732f44fe905950d78ddd00b58937441861
Dhruv Mehta <flashxx001@gmail.com>
2024-07-08 13:43:15 +0530

    Added Open and Close Functionality.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ const messages = [
   "Apply for jobs 💼",
   "Invest your new income ➰",
 ];
-// const step = 1;
 
 export default function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   console.log(messages.length);
   function handlePrevious() {
@@ -18,54 +18,64 @@ export default function App() {
     if (step < messages.length) setStep(step + 1);
   }
   return (
-    <div className="h-screen text-white bg-light-gray flex items-center justify-center ">
-      <div className="p-12 w-[600px] max-w-[90%] h-fit flex flex-col gap-8 items-center justify-between bg-zinc-200">
-        <div className="flex items-center justify-between w-full text-black ">
-          <div
-            className={`${
-              step >= 1 ? "bg-custom-purple" : "bg-zinc-300"
-            } rounded-full   font-semibold text-lg h-10 w-10 flex items-center justify-center cursor-pointer`}
-          >
-            1
+    <div className="h-screen text-white bg-light-gray flex items-center justify-center flex-col relative">
+      <p
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+        className="absolute top-[23%] text-black text-right w-[600px] max-w-[90%] text-3xl cursor-pointer"
+      >
+        &times;
+      </p>
+      {isOpen && (
+        <div className="p-12 w-[600px] max-w-[90%] h-fit flex flex-col gap-8 items-center justify-between bg-zinc-200">
+          <div className="flex items-center justify-between w-full text-black ">
+            <div
+              className={`${
+                step >= 1 ? "bg-custom-purple" : "bg-zinc-300"
+              } rounded-full   font-semibold text-lg h-10 w-10 flex items-center justify-center cursor-pointer`}
+            >
+              1
+            </div>
+            <div
+              className={`${
+                step >= 2 ? "bg-custom-purple" : "bg-zinc-300"
+              } rounded-full  w-10 font-semibold text-lg h-10 flex items-center justify-center cursor-pointer`}
+            >
+              2
+            </div>
+            <div
+              className={`${
+                step >= 3 ? "bg-custom-purple" : "bg-zinc-300"
+              } rounded-full  w-10 font-semibold text-lg h-10 flex items-center justify-center cursor-pointer`}
+            >
+              3
+            </div>
           </div>
-          <div
-            className={`${
-              step >= 2 ? "bg-custom-purple" : "bg-zinc-300"
-            } rounded-full  w-10 font-semibold text-lg h-10 flex items-center justify-center cursor-pointer`}
-          >
-            2
-          </div>
-          <div
-            className={`${
-              step >= 3 ? "bg-custom-purple" : "bg-zinc-300"
-            } rounded-full  w-10 font-semibold text-lg h-10 flex items-center justify-center cursor-pointer`}
-          >
-            3
+          <h1 className="font-semibold text-lg text-black">
+            Step {step} : {messages[step - 1]}
+          </h1>
+          <div className="flex items-center justify-between w-full">
+            <button
+              style={{ backgroundColor: "#7950f2" }}
+              className="font-semibold text-sm px-3 py-2 rounded-md"
+              onClick={handlePrevious}
+              // onMouseEnter={() => {
+              //   alert("Mouse has Entered.");
+              // }}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2" }}
+              className="font-semibold text-sm px-3 py-2 rounded-md"
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
-        <h1 className="font-semibold text-lg text-black">
-          Step {step} : {messages[step - 1]}
-        </h1>
-        <div className="flex items-center justify-between w-full">
-          <button
-            style={{ backgroundColor: "#7950f2" }}
-            className="font-semibold text-sm px-3 py-2 rounded-md"
-            onClick={handlePrevious}
-            // onMouseEnter={() => {
-            //   alert("Mouse has Entered.");
-            // }}
-          >
-            Previous
-          </button>
-          <button
-            style={{ backgroundColor: "#7950f2" }}
-            className="font-semibold text-sm px-3 py-2 rounded-md"
-            onClick={handleNext}
-          >
-            Next
-          </button>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
